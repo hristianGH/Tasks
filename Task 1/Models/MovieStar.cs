4368096b883fdd9cb5597b0a479a1f65e8d2967e
hristianGH <hristovhristian7@gmail.com>
2022-06-02 21:01:22 +0300

    Task1 Initial

---
+++ Task 1/Models/MovieStar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task_1.Models
+{
+    public class MovieStar
+    {
+        public DateTime DateOfBirth { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Sex { get; set; }
+        public string Nationality { get; set; }
+
+    }
+}
---
+++ Task 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using Newtonsoft;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText("Text\\input.txt");
+            Console.WriteLine(text);
+        }
+    }
+}
