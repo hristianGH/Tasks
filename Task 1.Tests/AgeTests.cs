743358e3cd2351458a6298096faa7e843903dc06
hristianGH <hristovhristian7@gmail.com>
2022-06-02 23:11:55 +0300

    Added Tests

---
+++ Task 1.Tests/AgeTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Task_1.Models;
+using Xunit;
+
+namespace Task_1.Tests
+{
+    public class AgeTests
+    {
+        [Fact]
+        public void GetAgeShouldReturnValue()
+        {
+            var star = new MovieStar()
+            {
+                Name = "Jon",
+                Surname = "Snow",
+                DateOfBirth = new DateTime(1000, 3, 22),
+                Nationality ="unknown",
+                Sex="Male"
+            };
+            var result = star.GetAge();
+            Assert.True(result > 0);
+        }
+
+        [Fact]
+        public void GetAgeShouldCheckMonthAndDateDiffrence()
+        {
+            var now = DateTime.Now;
+            var star = new MovieStar()
+            {
+                Name = "Some",
+                Surname = "Kid",
+                DateOfBirth = new DateTime(now.Year - 5, now.Month+1, now.Day),
+                Nationality = "unknown",
+                Sex = "Dunno"
+            };
+            var result = star.GetAge();
+            Assert.True(result == 4);
+            star = new MovieStar()
+            {
+                Name = "Other",
+                Surname = "Kid",
+                DateOfBirth = new DateTime(now.Year - 5, now.Month, now.Day+1),
+                Nationality = "unknown",
+                Sex = "Dunno"
+            };
+        }
+    }
+}
---
+++ Task 1.Tests/ToResultTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Task_1.Models;
+using Xunit;
+
+namespace Task_1.Tests
+{
+    public class ToResultTests
+    {
+        [Fact]
+        public void ToResultShouldReturnStringWithValue()
+        {
+            var star = new MovieStar()
+            {
+                Name = "Jon",
+                Surname = "Snow",
+                DateOfBirth = new DateTime(1000, 3, 22),
+                Nationality = "unknown",
+                Sex = "Male"
+            };
+            var result = star.ToResult();
+            Assert.NotEmpty(result);
+        }
+    }
+}
