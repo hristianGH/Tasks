3592027dd61f1d04f16ff9b351ea7dfc13b5f878
hristianGH <hristovhristian7@gmail.com>
2022-06-03 11:35:08 +0300

    Added Salary class and moved logic to class

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -6,42 +6,12 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            double salary = 0;
-            double tax = 0;
-            double socialContribution = 0;
+            var salary = new Salary(Console.ReadLine());
+            salary.Taxes();
 
-
-            //Check if input is valid
-            if (!double.TryParse(Console.ReadLine(), out salary) || salary < 0)
-            {
-                Console.WriteLine("Invalid value !");
-                return;
-            }
-
-            //Check amount lower than 1000
-            if (salary <= 1000)
-            {
-                Console.WriteLine("Income below threshold, no tax");
-                return;
-            }
-
-            tax = (salary - 1000) * 0.10;
-            Console.WriteLine($"Income above threshold, 10% tax = {tax} IDR");
-            salary -= tax;
-
-            //Social Contributions 
-            if (salary-1000 <= 3000)
-            {
-                socialContribution = (salary - 1000) * 0.15;
-                Console.WriteLine($"Social contribution of 15% = {socialContribution} IDR");
-                salary -= socialContribution;
-            }
-            Console.WriteLine($"Total tax = {tax + socialContribution}");
-            Console.WriteLine($"Income is {salary}");
-
-            //Comment for additional taxes 
-            //Higher paid individuals are required a tax of 25% to social contributions  
-            //Higher paid individuals are considered those with 3000 and over monthly income
+            // Comment for additional taxes 
+            // Higher paid individuals are considered those with 3000 and over monthly income
+            // Higher paid individuals are required a tax of 25% to social contributions  
         }
     }
 }
---
+++ Task2/Salary.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Task2
+{
+    public class Salary
+    {
+        private bool salaryIsValid = true;
+        private double income;
+        private double baseIncome = 1000;
+
+        public Salary(double salary)
+        {
+            // Check if input is valid
+            if (salary < 0)
+            {
+                salaryIsValid = false;
+                return;
+            }
+            this.income = salary;
+
+        }
+        public Salary(string salaryConsole)
+        {
+            // Check if input is valid
+            double salary = 0;
+            bool isValid = double.TryParse(salaryConsole, out salary);
+
+            if (salary < 0 || !isValid)
+            {
+                Console.WriteLine("Invalid value !");
+                salaryIsValid = false;
+                return;
+            }
+            this.income = salary;
+        }
+
+        public double Income { get { return this.income; } }
+        public bool SalaryIsValid { get { return this.salaryIsValid; } }
+
+        public double Tax { get; set; }
+        public double SocialContribution { get; set; }
+
+        public void Taxes()
+        {
+            if (!salaryIsValid)
+            {
+                return;
+            }
+
+            ApplyTax();
+            SocialContributing();
+
+            Console.WriteLine($"Total tax = {Tax + SocialContribution}");
+            Console.WriteLine($"Income is {income}");
+        }
+        private void ApplyTax()
+        {
+            // Check amount lower than 1000
+            if (income <= baseIncome)
+            {
+                Console.WriteLine("Income below threshold, no tax");
+                return;
+            }
+
+            Tax = (income - baseIncome) * 0.10;
+            Console.WriteLine($"Income above threshold, 10% tax = {Tax} IDR");
+            income -= Tax;
+        }
+        private void SocialContributing()
+        {
+            if (income - 1000 > 3000)
+            {
+                return;
+            }
+            SocialContribution = (income - baseIncome) * 0.15;
+            Console.WriteLine($"Social contribution of 15% = {SocialContribution} IDR");
+            income -= SocialContribution;
+
+        }
+    }
+}
